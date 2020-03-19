1fad67a278ac98063d45ff92c6d6ee5e22e8b356
kangkang <824335342@qq.com>
2020-03-19 16:27:37 +0800

    添加了CAS

---
+++ thread-one/src/main/java/com/abu/cas/CASdemo.java
@@ -0,0 +1,21 @@
+package com.abu.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 比较并交换
+ */
+public class CASdemo {
+
+    public static void main(String[] args) {
+
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+        boolean flag = atomicInteger.compareAndSet(5, 100);
+        System.out.println(flag);
+
+        System.out.println("===========");
+
+        boolean flag02 = atomicInteger.compareAndSet(5, 2020);
+        System.out.println(flag02);
+    }
+}
