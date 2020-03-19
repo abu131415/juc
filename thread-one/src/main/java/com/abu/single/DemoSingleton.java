4a61cca11117828d14e547f5603875687509ede7
kangkang <824335342@qq.com>
2020-03-19 15:54:31 +0800

    第四次提交

--- thread-one/src/main/java/com/abu/single/DemoSingleton.java
+++ thread-one/src/main/java/com/abu/single/DemoSingleton.java
@@ -2,15 +2,24 @@ package com.abu.single;
 
 public class DemoSingleton {
 
+    //    禁止指令重排
+    private volatile static DemoSingleton demoSingleton;
+
     private DemoSingleton() {
         System.out.println("实例被初始化了！！");
     }
 
-    private static DemoSingleton demoSingleton;
-
+    /**
+     * 双重检锁机制
+     * @return
+     */
     public static DemoSingleton getDemoSingleton() {
         if (demoSingleton == null) {
-            demoSingleton = new DemoSingleton();
+            synchronized (DemoSingleton.class) {
+                if (demoSingleton == null) {
+                    demoSingleton = new DemoSingleton();
+                }
+            }
         }
         return demoSingleton;
     }
@@ -21,7 +30,7 @@ public class DemoSingleton {
 //        DemoSingleton demoSingleton01 = DemoSingleton.getDemoSingleton();
 //        DemoSingleton demoSingleton02 = DemoSingleton.getDemoSingleton();
 //      线程不安全
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             new Thread(() -> {
                 DemoSingleton.getDemoSingleton();
             }).start();
