af314f13d33485411be5872d18cd9af84eb84b7b
kangkang <824335342@qq.com>
2020-03-19 13:44:11 +0800

    第一次提交

---
+++ thread-one/src/main/java/com/abu/One.java
@@ -0,0 +1,10 @@
+package com.abu;
+
+public class One {
+
+        volatile int num = 0;
+
+        public void add() {
+            this.num = 60;
+        }
+    }
---
+++ thread-one/src/main/java/com/abu/ThreadVisual.java
@@ -0,0 +1,25 @@
+package com.abu;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadVisual {
+
+    public static void main(String[] args) {
+        One one = new One();
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            one.add();
+            System.out.println(Thread.currentThread().getName() + "==" + one.num);
+        }).start();
+
+        while (one.num==0){
+
+        }
+
+        System.out.println(Thread.currentThread().getName()+"thread is over");
+    }
+}
